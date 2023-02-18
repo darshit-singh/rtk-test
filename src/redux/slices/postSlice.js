f628595e6e6ad0bf4a5adc7987e2292b9d296540
Darshit Singh Sarna <op.dare7@gmail.com>
2023-02-18 22:11:10 +0530

    updating initial state for getting data asynchronously

--- src/redux/slices/postSlice.js
+++ src/redux/slices/postSlice.js
@@ -1,34 +1,11 @@
 import { createSlice, nanoid } from "@reduxjs/toolkit";
 import { sub } from "date-fns";
 
-const initialState = [
-    {
-        id: '1',
-        title: 'Learning Redux Toolkit',
-        content: "I've heard good things.",
-        date: sub(new Date(), { minutes: 10 }).toISOString(),
-        reactions: {
-            thumbsUp: 0,
-            wow: 0,
-            heart: 0,
-            rocket: 0,
-            coffee: 0
-        }
-    },
-    {
-        id: '2',
-        title: 'Slices...',
-        content: "The more I say slice, the more I want pizza.",
-        date: sub(new Date(), { minutes: 5 }).toISOString(),
-        reactions: {
-            thumbsUp: 0,
-            wow: 0,
-            heart: 0,
-            rocket: 0,
-            coffee: 0
-        }
-    }
-]
+const initialState = {
+    posts: [],
+    status: 'idle', // 'idle' | 'loading' | 'succeeded' | 'failed'
+    error: null
+}
 //can directly set state inside createslice as it uses immer.js under the hood so push like this doesn't mutate the state
 const postSlice = createSlice({
     name: "posts",
@@ -36,7 +13,7 @@ const postSlice = createSlice({
     reducers: {
         postAdded: {
             reducer: (state, action) => {
-                state.push(action.payload)
+                state.posts.push(action.payload)
             },
             prepare: (title, content, userId) => {
                 const id = nanoid()
@@ -60,7 +37,7 @@ const postSlice = createSlice({
         },
         reactionAdded: (state, action) => {
             const { postId, reaction } = action.payload
-            const existingPost = state.find(post => post.id === postId)
+            const existingPost = state.posts.find(post => post.id === postId)
             if (existingPost) {
                 //this would normally mutate the state but since we're inside createslice, immerjs takes care of it
                 existingPost.reactions[reaction]++
@@ -69,7 +46,8 @@ const postSlice = createSlice({
     }
 })
 
-export const selectPosts = (state) => state.posts
+//always export select here, cuz if the shape of the state changes, you only have to make changes in this file, not everywhere else.
+export const selectPosts = (state) => state.posts.posts //posts array inside the initial state
 
 export const postActions = { ...postSlice.actions }
 
