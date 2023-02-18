516ee73cd57366c66b3d9af756ca6a99d8ce76ed
Darshit Singh Sarna <darshit.sarna@bytelearn.ai>
2023-02-18 18:18:05 +0530

    user id getting updated with each post

--- src/components/AddPostForm.jsx
+++ src/components/AddPostForm.jsx
@@ -5,21 +5,37 @@ import { selectUsers } from "../redux/slices/userSlice"
 
 const AddPostForm = () => {
   const dispatch = useDispatch()
+
   const [title, setTitle] = useState("")
   const [content, setContent] = useState("")
+  const [userId, setUserId] = useState("")
+
+  const users = useSelector(selectUsers)
 
   const onTitleChange = (e) => setTitle(e.target.value)
   const onContentChange = (e) => setContent(e.target.value)
+  const onAuthorChange = (e) => setUserId(e.target.value)
 
   const onSavePostClicked = () => {
     if (title && content) {
       dispatch(
-        postActions.postAdded(title, content)
+        postActions.postAdded(title, content, userId)
       )
       setTitle("")
       setContent("")
+      setUserId("")
     }
   }
+
+  const usersOptions = users.map((user) => {
+    return (
+      <option key={user.id} value={user.id}>
+        {user.name}
+      </option>
+    )
+  })
+
+  const canSave = Boolean(title) && Boolean(content) && Boolean(userId)
 
   return (
     <section>
@@ -28,18 +44,28 @@ const AddPostForm = () => {
         <label htmlFor="postTitle">Post Title:</label>
         <input
           type="text"
+          id="postTitle"
           name="postTitle"
           value={title}
           onChange={onTitleChange}
         />
+
+        <label htmlFor="postAuthor">Author:</label>
+        <select id="postAuthor" value={userId} onChange={onAuthorChange}>
+          <option value=""></option>
+          {usersOptions}
+        </select>
+
         <label htmlFor="postContent">Post Content:</label>
         <textarea
           type="text"
+          id="postContent"
           name="postContent"
           value={content}
           onChange={onContentChange}
         />
-        <button type="button" onClick={onSavePostClicked}>Save Post</button>
+
+        <button type="button" onClick={onSavePostClicked} disabled={!canSave}>Save Post</button>
       </form>
     </section>
   )
--- src/redux/index.js
+++ src/redux/index.js
@@ -1,13 +1,13 @@
 import { configureStore } from "@reduxjs/toolkit";
 import counterReducer from "./slices/counterSlice";
 import postsReducer from "./slices/postSlice";
-import userSlice from "./slices/userSlice";
+import usersReducer from "./slices/userSlice";
 
 const store = configureStore({
     reducer: {
         counter: counterReducer,
         posts: postsReducer,
-        users: userSlice,
+        users: usersReducer,
     }
 })
 
