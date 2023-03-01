3cc30055fad3effe1565f7b5586d0397a1d3cd96
Darshit Singh Sarna <op.dare7@gmail.com>
2023-03-01 23:56:28 +0530

    fetch users added

--- src/main.jsx
+++ src/main.jsx
@@ -3,12 +3,15 @@ import ReactDOM from 'react-dom/client'
 import { Provider } from 'react-redux'
 import App from './App'
 import './index.css'
-import store from './redux'
+import store from './redux/index'
+import { userActions } from './redux/slices/userSlice'
+
+store.dispatch(userActions.fetchUsers())
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <Provider store={store}>
-     <App />
+      <App />
     </Provider>
   </React.StrictMode>,
 )
--- src/redux/slices/userSlice.js
+++ src/redux/slices/userSlice.js
@@ -3,22 +3,29 @@ import axios from "axios";
 
 const USERS_URL = 'https://jsonplaceholder.typicode.com/users' // could instead create an axios instance instead of base url
 
-const initialState = [
-    { id: '0', name: 'Darshit Singh' },
-    { id: '1', name: 'test name 2' },
-    { id: '2', name: 'test name 3' }
-]
+const initialState = []
+
+const fetchUsers = createAsyncThunk('users/fetchUsers', async () => {
+    const response = await axios.get(USERS_URL)
+    return response.data
+})
 
 const userSlice = createSlice({
     name: 'users',
     initialState,
-    reducers: {
-
+    reducers: {},
+    extraReducers(builder) {
+        builder.addCase(fetchUsers.fulfilled, (state, action) => {
+            return action.payload   //this means we're completely overwriting the state. 
+        })
     }
 })
 
 export const selectUsers = state => state.users
 
-export const userActions = { ...userSlice.actions }
+export const userActions = {
+    ...userSlice.actions,
+    fetchUsers
+}
 
 export default userSlice.reducer
