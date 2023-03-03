6613a8ec7ea36d6d9c48d3926b7b9458099bb89a
Darshit Singh Sarna <op.dare7@gmail.com>
2023-03-03 23:48:56 +0530

    added routes in index

--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import { Provider } from 'react-redux'
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
 import App from './App'
 import './index.css'
 import store from './redux/index'
@@ -11,7 +12,11 @@ store.dispatch(userActions.fetchUsers())
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <Provider store={store}>
-      <App />
+      <Router>
+        <Routes>
+          <Route path="/" element={<App />} />
+        </Routes>
+      </Router>
     </Provider>
   </React.StrictMode>,
 )
