9f979a3b58cfea72b5820a6aac0d2421320a7d86
Darshit Singh Sarna <op.dare7@gmail.com>
2023-03-05 23:52:21 +0530

    path typo

--- src/main.jsx
+++ src/main.jsx
@@ -14,7 +14,7 @@ ReactDOM.createRoot(document.getElementById('root')).render(
     <Provider store={store}>
       <Router>
         <Routes>
-          <Route path="/" element={<App />} />
+          <Route path="/*" element={<App />} />
         </Routes>
       </Router>
     </Provider>
